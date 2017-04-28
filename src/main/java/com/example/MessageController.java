78d60ddc3c0bcdcf1b11099ec6f2a6aabdea35c4
Keith Cummins n0266162 <keith.cummins@liberty-it.co.uk>
2017-04-28 20:52:35 +0100

    initial checkin

---
+++ src/main/java/com/example/MessageController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+    @GetMapping
+    public String msg() {
+        return "Hello Jenkins";
+    }
+}
